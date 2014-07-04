ecde5c8782603da27082a9e3eaa45d59d6f5e365
m-zar <m.zar@gmx.de>
2014-07-04 15:12:26 +0200

    starting uwsgi from application dir

--- modules/uwsgi/manifests/init.pp
+++ modules/uwsgi/manifests/init.pp
@@ -8,6 +8,7 @@ class uwsgi ($app_path, $virtualenv_name) {
   exec { 'start uwsgi':
     command => "${app_path}/${virtualenv_name}/bin/uwsgi --socket 127.0.0.1:8080 -w WSGI:app &",
     user    => 'vagrant',
+    cwd     => $app_path,
     require => Exec['virtualenv', 'install uwsgi', 'install flask'],
   }
 }
